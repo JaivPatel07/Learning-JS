b0d32c04a6fd526adf9d5749648f9f84ef412cda
Jaiv Patel <jaivpatel402@gmail.com>
2025-11-12 16:35:03 +0000

    Fix comment for clarity on gameName usage in 05_string.js

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com') // open this in browser 
+const gameName = new String('hitesh-hc-com') // open and run this in browser 
 // sample shown in 05_string pngs
 
 // console.log(gameName[0]);
