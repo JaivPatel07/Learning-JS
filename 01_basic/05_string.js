a701d3706a1d9c15fcc20aa88fdb6dac2e97f386
Jaiv Patel <jaivpatel402@gmail.com>
2025-11-12 16:33:41 +0000

    Refactor code structure for improved readability and maintainability

---
+++ 01_basic/05_string.js
@@ -0,0 +1,39 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com') // open this in browser 
+// sample shown in 05_string pngs
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);// It shows the prototype of the string
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4) //It supports negative numbers, but they count from the end of the string, not as real negative indexes.
+console.log(anotherString);
+
+// in js only at() use -ve indexing 
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
